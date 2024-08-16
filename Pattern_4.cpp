16ce536ae451f89117c6912359c7386843503ebb
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-08-17 00:58:02 +0530

    Pattern_4.cpp

---
+++ Pattern_4.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void pattern4(int n) {
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                cout<<i;
+            }
+            cout<<endl;
+        }
+    }
+};
