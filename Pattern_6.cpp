a401acb33127f79d9fc2d14dad738d78a0dc5321
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-08-17 02:48:47 +0530

    Pattern_6.cpp

---
+++ Pattern_6.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void pattern6(int n) {
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=(n-i+1); j++){
+                cout<<j;
+            }
+            cout<<endl;
+        }
+    }
+};
