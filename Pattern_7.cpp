292501af5acda9d886be6a09338cfbd05a349dab
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-08-18 17:27:58 +0530

    Pattern_7.cpp

---
+++ Pattern_7.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void pattern7(int n) {
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                cout<<" ";
+            }
+            for(int j=1; j<=2*i-1; j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+    }
+};
