c4b317e9705189edb0ba29745f2e87ab0cac2e16
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-09-14 18:29:48 +0530

    Switch_Case.cpp

---
+++ Switch_Case.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void whichWeekDay(int day) {
+        switch(day){
+            case 1: cout<<"Monday"<<endl;
+            break;
+            case 2: cout<<"Tuesday"<<endl;
+            break;
+            case 3: cout<<"Wednesday"<<endl;
+            break;
+            case 4: cout<<"Thursday"<<endl;
+            break;
+            case 5: cout<<"Friday"<<endl;
+            break;
+            case 6: cout<<"Saturday"<<endl;
+            break;
+            case 7: cout<<"Sunday"<<endl;
+            break;
+            default : cout<<"Invalid"<<endl;
+            break;
+        }
+    }
+};
