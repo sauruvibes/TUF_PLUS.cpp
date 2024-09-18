0e2a21ea2b8688079173a0e13c12c4ec3823b676
Saurabh Sonune <119730715+sauruvibes@users.noreply.github.com>
2024-09-18 14:12:31 +0530

    Count_all_digits_of_a_number.cpp

---
+++ Count_all_digits_of_a_number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countDigit(int n) {
+        if(n == 0){
+            return 1;
+        }
+
+        int count = 0;
+        while(n > 0){
+            int lastDig = n % 10;
+            count = count+1;
+            n = n/10;
+        }
+        return count;
+    }
+};
